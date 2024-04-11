593a772e715dc9f28540ee509d787bacbf14df44
nataliaschmidt <nataliak.schmidt@gmail.com>
2024-04-11 02:42:38 -0300

    cria a rota de GET /jobs realizando a buscar de trabalho  por parametro de level de senioridade, caso não haja um parametro na URL retorna todos os trabalhos

--- desafio_naranjalabs/src/app/(api)/jobs/route.ts
+++ desafio_naranjalabs/src/app/(api)/jobs/route.ts
@@ -1,9 +1,17 @@
+import { type NextRequest } from 'next/server';
 import jobs from "../database/jobs";
+import { IJob } from '../Interfaces/IJob';
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+
+  const searchParams = req.nextUrl.searchParams;
+  const query = searchParams.get('level')?.toLowerCase();
 
   try {
-
+    if (query) {
+      const foundQuery = jobs.filter((job: IJob) => job.level.toLowerCase()  === query);
+      return Response.json(foundQuery);
+    }
     return Response.json(jobs);
 
   } catch (error) {
